b375ed3f83013b4aa6b7f4793f917d08310ee456
Kalyani-Shebe <kalyanishebe02@gmail.com>
2026-05-26 10:15:05 +0000

    initial airflow dag

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hello from GitSync DAG")
+
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2024,1,1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello
+    )
